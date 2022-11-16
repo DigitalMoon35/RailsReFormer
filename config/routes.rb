1170fc606d8d37085faee2c4d6745914dc710bbb
DigitalMoon35 <freshlife35@yahoo.com>
2022-11-16 13:29:01 -0500

    created User controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :users, only: [:new, :create]
+  # get '/new', to: 'user#new'
   # Defines the root path route ("/")
   # root "articles#index"
 end
